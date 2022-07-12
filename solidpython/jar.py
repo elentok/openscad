9d019bb23b9e1c81d308dc171c339b3847d1fa2d
David Elentok <3david@gmail.com>
2022-07-12 16:01:43 +0300

    fix(jar): wip

--- solidpython/jar.py
+++ solidpython/jar.py
@@ -1,4 +1,5 @@
 from solid import *
+from lib.render import render
 
 from solid.extensions.bosl2 import threading
 
@@ -14,10 +15,29 @@ class Jar:
         self.thread_height = thread_height or 0.2 * self.height
 
     def render_body(self):
-        return cylinder(d=self.diameter, h=self.height) - threading.threaded_rod(
-            d=self.diameter - self.wall_width, l=self.thread_height
+        jar = cylinder(d=self.diameter, h=self.wall_width) + hollow_cylinder(
+            self.diameter, self.height, self.wall_width
         )
+
+        return jar
+
+        # thread = cylinder(
+        #     d=self.diameter, h=self.thread_height, center=True
+        # ) - threading.threaded_rod(
+        #     d=self.diameter - self.wall_width,
+        #     l=self.thread_height + 0.1,
+        #     pitch=self.thread_height / 5,
+        #     internal=True,
+        # )
+        # return jar + thread
+        # return jar - thread
+
+
+def hollow_cylinder(d: float, h: float, thickness: float):
+    return cylinder(d=d, h=h, center=True) - cylinder(
+        d=d - thickness * 2, h=h + 0.2, center=True
+    )
 
 
 jar = Jar(diameter=25, height=30, wall_width=2)
-scad_render_to_file(jar.render_body(), file_header="$fn=60;\n")
+render(jar.render_body())
