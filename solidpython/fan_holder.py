22ddff6e18732b199a997de6c5c72220633becab
David Elentok <3david@gmail.com>
2022-07-12 13:41:08 +0300

    fix(fan_holder): fixes

--- solidpython/fan_holder.py
+++ solidpython/fan_holder.py
@@ -5,48 +5,54 @@ from solid import *
 from lib.chamfer import chamfered_hole
 
 # Settings for a 92mm fan
-distance_between_holes = 82.5
-screw_hole_diameter = 4.4
+fan_hole_distance = 82.5
+fan_screw_hole_diameter = 4.4
 chamfer_diameter = 9
-padding = 3
-grip_diameter = screw_hole_diameter + padding * 2
+plate_thickness = 2
+padding = 3.1
+grip_padding = 2
+grip_diameter = fan_screw_hole_diameter + grip_padding * 2
 width = 16.3
-hole_distance = (distance_between_holes - grip_diameter / 2) / 2 * 0.75
+wall_hole_distance = (fan_hole_distance - grip_diameter / 2) / 2 * 0.75
 
 
 def fan_holder():
     return (
-        flat_fan_holder().linear_extrude(width, center=True)
-        - hole().left(hole_distance)
-        - hole().right(hole_distance)
+        flat_fan_holder()
+        .linear_extrude(width, center=True)
+        .forward(fan_screw_hole_diameter / 2 + padding * 2)
+        - hole().left(wall_hole_distance)
+        - hole().right(wall_hole_distance)
     )
 
 
 def hole():
     return (
-        chamfered_hole(padding + 0.2, screw_hole_diameter, chamfer_diameter)
+        chamfered_hole(padding + 0.2, fan_screw_hole_diameter, chamfer_diameter)
         .rotateX(-90)
-        .back(grip_diameter / 2 - padding / 2)
+        .forward(padding / 2)
     )
 
 
 def flat_fan_holder():
     return (
-        grip().left(distance_between_holes / 2)
-        + grip().right(distance_between_holes / 2)
-        + square([distance_between_holes, padding], center=True).back(
-            grip_diameter / 2 - padding / 2
+        grip().left(fan_hole_distance / 2)
+        + grip().right(fan_hole_distance / 2)
+        + square([fan_hole_distance + grip_diameter, padding], center=True).back(
+            padding * 1.5 + fan_screw_hole_diameter / 2
         )
     )
 
 
 def grip():
     return hull()(
-        square(size=[grip_diameter, padding], center=True).back(
-            grip_diameter / 2 - padding / 2
+        debug()(
+            square(size=[grip_diameter, padding], center=True).back(
+                padding / 2 + fan_screw_hole_diameter / 2
+            )
         )
         + circle(d=grip_diameter)
-    ) - circle(d=screw_hole_diameter)
+    ) - circle(d=fan_screw_hole_diameter)
 
 
 scad_render_to_file(fan_holder(), file_header=f"$fn = 50;")
