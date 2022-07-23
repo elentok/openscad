0c592af6c4531fcf2dbeb5ff1997852ea9d48483
David Elentok <3david@gmail.com>
2022-07-23 18:04:19 +0300

    refactor(toy_scope): cleanup

--- solidpython/toy_scope.py
+++ solidpython/toy_scope.py
@@ -14,26 +14,26 @@ class Scope:
         self.r1 = r1
         self.r2 = r2
         self.r3 = r3
+        self.l1 = self.length * 0.2
+        self.l1to2 = self.length * 0.1
+        self.l2 = self.length * 0.3
+        self.l2to3 = self.length * 0.1
 
     def render(self):
-        return self.render2d().rotateZ(90).rotate_extrude()
+        return self.render2d().rotate_extrude()
 
     def render2d(self):
-        l1 = self.length * 0.2
-        l1to2 = self.length * 0.1
-        l2 = self.length * 0.3
-        l2to3 = self.length * 0.1
         return rounded_polyline(
             thickness=2,
             points=[
                 (0, self.r1),
-                (l1, self.r1),
-                (l1 + l1to2, self.r2),
-                (l1 + l1to2 + l2, self.r2),
-                (l1 + l1to2 + l2 + l2to3, self.r3),
+                (self.l1, self.r1),
+                (self.l1 + self.l1to2, self.r2),
+                (self.l1 + self.l1to2 + self.l2, self.r2),
+                (self.l1 + self.l1to2 + self.l2 + self.l2to3, self.r3),
                 (self.length, self.r3),
             ],
-        )
+        ).rotateZ(90)
 
 
 if __name__ == "__main__":
