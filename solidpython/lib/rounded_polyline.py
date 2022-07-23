2d2a76610b07d9edd4ddf6758743e796e7c34473
David Elentok <3david@gmail.com>
2022-07-23 18:00:54 +0300

    feat(lib): add rounded_polyline

---
+++ solidpython/lib/rounded_polyline.py
@@ -0,0 +1,16 @@
+from typing import List, Tuple
+from solid import *
+
+
+def rounded_polyline(thickness: float, points: List[Tuple[float, float]]):
+    lines = []
+    for i in range(len(points) - 1):
+        (x1, y1) = points[i]
+        (x2, y2) = points[i + 1]
+        line = hull()(
+            circle(d=thickness).right(x1).forward(y1),
+            circle(d=thickness).right(x2).forward(y2),
+        )
+        lines.append(line)
+
+    return union()(lines).right(thickness / 2)
