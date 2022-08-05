e3379ebede94ec0209cad05552c2695de6d51bd4
David Elentok <3david@gmail.com>
2022-08-05 14:28:54 +0300

    fix(pegboard): improvements

--- solidpython/pegboard.py
+++ solidpython/pegboard.py
@@ -19,6 +19,9 @@ class Pegboard:
     border_width: float = 2
     border_corner_percentage: float = 0.3
 
+    def peg_diameter(self):
+        return self.hole_diameter - 0.8
+
     def width(self):
         return (
             (self.holes_x - 1) * self.hole_spacing
@@ -80,4 +83,114 @@ class Pegboard:
         return union()(holes)
 
 
-render(Pegboard().render())
+@dataclass
+class PegboardMount:
+    pegboard: Pegboard
+
+    def render(self):
+        return union()(
+            self.render_bar(),
+            self.render_pegs(),
+        )
+
+    def render_pegs(self):
+        p = self.pegboard
+        return union()(
+            self.render_top_peg().right(p.thickness / 2),
+            self.render_bottom_peg().right(p.peg_diameter() / 2),
+        )
+
+    def render_bar(self):
+        p = self.pegboard
+        width = p.peg_diameter() * 2  # TODO
+        height = p.hole_spacing + p.hole_diameter * 2
+        return (
+            RoundedSquare(Size2D(width, height), Radius(p.hole_diameter / 2))
+            .render()
+            .linear_extrude(p.thickness, center=True)
+            .rotateY(90)
+        )
+
+    def render_top_peg(self):
+        p = self.pegboard
+        radius = p.peg_diameter() + p.thickness
+        arc = (
+            circle(d=p.peg_diameter())
+            .left(radius)
+            .rotate_extrude(90)
+            .rotateZ(90)
+            .forward(radius)
+        )
+        end = sphere(d=p.peg_diameter()).forward(radius).right(radius)
+        return union()(arc, end).forward(p.hole_spacing / 2)
+
+    def render_bottom_peg(self):
+        p = self.pegboard
+        sphere1 = sphere(d=p.peg_diameter())
+        sphere2 = sphere1.right(p.thickness)
+        return hull()(
+            sphere1,
+            sphere2,
+        ).back(p.hole_spacing / 2)
+
+    def render2d(self):
+        p = self.pegboard
+        peg_radius = p.peg_diameter() / 2
+
+        bar_width = p.thickness
+        bar_height = 2 * p.hole_spacing + p.hole_diameter
+        bar = RoundedSquare(
+            Size2D(bar_width, bar_height),
+            Radius(2),
+        ).render()
+
+        top_peg_width = p.thickness + p.wall_distance / 2
+        # top_peg = (
+        #     hull()(
+        #         square([p.peg_diameter(), p.peg_diameter()], center=True),
+        #         circle(d=p.peg_diameter()).right(top_peg_width - peg_radius),
+        #     )
+        #     .right(peg_radius + bar_width / 2)
+        #     .forward(p.hole_spacing / 2)
+        # )
+        top_peg_part1 = RoundedSquare(
+            Size2D(top_peg_width, p.peg_diameter()),
+            Radius(0, 0, peg_radius, 0),
+        ).render()
+
+        top_peg_part2 = (
+            RoundedSquare(
+                Size2D(p.peg_diameter(), p.peg_diameter()),
+                Radius(peg_radius, peg_radius, 0, 0),
+            )
+            .render()
+            .right(top_peg_width / 2 - peg_radius)
+            .forward(p.peg_diameter())
+        )
+
+        top_peg = (
+            union()(top_peg_part1, top_peg_part2)
+            .right(top_peg_width / 2 + bar_width / 2)
+            .forward(p.hole_spacing / 2)
+        )
+
+        bottom_peg = (
+            RoundedSquare(
+                Size2D(p.thickness, p.peg_diameter()),
+                Radius(0, peg_radius, peg_radius, 0),
+            )
+            .render()
+            .back(p.hole_spacing / 2)
+            .right(p.thickness)
+        )
+
+        return union()(bar, top_peg, bottom_peg)
+
+
+# @dataclass
+# class PegboardHook:
+
+
+pegboard = Pegboard()
+# render(Pegboard().render())
+render(PegboardMount(pegboard).render())
